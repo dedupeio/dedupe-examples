6cd8d6650e33c1235fe2e24aa14e94d7600c87d5
Cathy Deng <cathyd125@gmail.com>
2014-07-10 13:52:57 -0500

    modify getCentroid so that it can take in different comparators, fix errors in getCentroid & getCanonicalRep, break centroid ties

--- csv_example/csv_example.py
+++ csv_example/csv_example.py
@@ -20,6 +20,7 @@ import re
 import collections
 import logging
 import optparse
+import numpy as np
 from numpy import nan
 from numpy import matlib
 
@@ -182,14 +183,18 @@ print '# duplicate sets', len(clustered_dupes)
 
 #Canonicalization###########################################################################
 
-# testing #####
-dupe_cluster = clustered_dupes[0]
-
-# calculate centroid w/ affine gaps
+# takes in a list of attribute values for a field,
+# evaluates the centroid using the comparator,
+# & returns the centroid (i.e. the 'best' value for the field)
 def getCentroid( attribute_variants, comparator ):
-    n = len(attribute_variants) # *** if n = 0 (i.e. all values were empty & ignored), return ''
-    dist_matrix = numpy.matlib.zeros([n,n])
-    avg_dist = numpy.zeros(n)
+    n = len(attribute_variants)
+    # if all values were empty & ignored, return ''
+    if n == 0:
+        return ''
+    if n == 1:
+        return attribute_variants[0]
+    dist_matrix = np.matlib.zeros([n,n])
+    avg_dist = np.zeros(n)
     # this is a matrix of distances between all strings
     # populate distance matrix by looping through elements of matrix triangle
     for i in range (1,n):
@@ -201,26 +206,32 @@ def getCentroid( attribute_variants, comparator ):
     for i in range (1,n):
         avg_dist = dist_matrix.mean(0)
     # find string with min avg distance
-    min_dist = avg_dist.argmin()
-    min_dist_indices = numpy.where(avg_dist==min_dist)
+    avg_dist = np.array(avg_dist)
+    min_dist_indices = np.where(avg_dist==avg_dist.min())
     # if there is only one value w/ min avg dist
-    if len(min_dist_indices[0])==1:
-        centroid_index = min_dist_indices[0][0]
+    if len(min_dist_indices[1])==1:
+        centroid_index = min_dist_indices[1][0]
         return attribute_variants[centroid_index]
     # if there are multiple values w/ min avg dist
     else:
-        return breakCentroidTie( attribute_variants, min_dist_indices ) # *** what should happen here with different data types?
+        return breakCentroidTie( attribute_variants, min_dist_indices )
 
 # find centroid when there are multiple values w/ min avg distance (e.g. any dupe cluster of 2)
-def breakCentroidTie( attribute_variants, min_dist_indices ): #################################
-###############################################################################################
+# right now this just selects the first among a set of ties
+# TO-DO? set this up so that for strings, it breaks ties by selecting the longest string (would need to take in comparator)
+def breakCentroidTie( attribute_variants, min_dist_indices ):
+    return attribute_variants[min_dist_indices[1][0]]
 
+#this is the string distance calculation
+from dedupe.distance.affinegap import normalizedAffineGapDistance as comparator
 
 # takes in a cluster of duplicates & data, returns canonical representation of cluster
-# *** get this to take in data model, so that it knows data types, comparators
+# TO-DO: get this to take in data model, so that it knows data types, comparators
 def getCanonicalRep( dupe_cluster, data_d):
     keys = data_d[0].keys()
     canonical_rep = dict()
+    ####### TO-DO ############
+    # comparator = 
 
     #loop through keys & values in data, get centroid for each key
     for key in keys:
@@ -229,14 +240,9 @@ def getCanonicalRep( dupe_cluster, data_d):
             #ignore empty values (assume non-empty values always better than empty value for canonical record)
             if data_d[record_id][key] != '':
                 key_values.append(data_d[record_id][key])
-        canonical_rep[key] = getCentroid(key_values) # *** add comparator
+        canonical_rep[key] = getCentroid(key_values, comparator)
     return canonical_rep
 
-#this is the string distance calculation
-from dedupe.distance.affinegap import normalizedAffineGapDistance as comparator
-
-#call getCanonicalRep
-#getCanonicalRep(dupe_cluster, data_d)
 
 #############################################################################################
 
