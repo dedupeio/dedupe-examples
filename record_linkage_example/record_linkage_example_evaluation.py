7440193a7cf2b0745e052cf0551a4b23cbb02160
Forest Gregg <fgregg@uchicago.edu>
2017-03-14 09:34:23 -0500

    requirements and python3 compatibility

--- record_linkage_example/record_linkage_example_evaluation.py
+++ record_linkage_example/record_linkage_example_evaluation.py
@@ -8,14 +8,14 @@ def evaluateDuplicates(found_dupes, true_dupes):
     false_positives = found_dupes.difference(true_dupes)
     uncovered_dupes = true_dupes.difference(found_dupes)
 
-    print 'found duplicate'
-    print len(found_dupes)
+    print('found duplicate')
+    print(len(found_dupes))
 
-    print 'precision'
-    print 1 - len(false_positives) / float(len(found_dupes))
+    print('precision')
+    print(1 - len(false_positives) / float(len(found_dupes)))
 
-    print 'recall'
-    print len(true_positives) / float(len(true_dupes))
+    print('recall')
+    print(len(true_positives) / float(len(true_dupes)))
 
 
 def dupePairs(filename, rowname) :
@@ -30,7 +30,7 @@ def dupePairs(filename, rowname) :
         del dupe_d['x']
 
     dupe_s = set([])
-    for (unique_id, cluster) in dupe_d.iteritems():
+    for (unique_id, cluster) in dupe_d.items():
         if len(cluster) > 1:
             for pair in itertools.combinations(cluster, 2):
                 dupe_s.add(frozenset(pair))
