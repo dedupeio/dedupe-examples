d1cc937724edc9b444b25dec616b4cce39a3d21a
Forest Gregg <fgregg@uchicago.edu>
2018-09-13 00:50:45 -0500

    officers work

--- extended-variables/officers.py
+++ extended-variables/officers.py
@@ -8,10 +8,8 @@ from future.builtins import next
 import os
 import csv
 import re
-import collections
 import logging
 import optparse
-from numpy import nan
 
 import dedupe
 from unidecode import unidecode
@@ -66,7 +64,7 @@ def readData(filename):
     with open(filename) as f:
         reader = csv.DictReader(f)
         for row in reader:
-            clean_row = dict([(k, preProcess(v)) for (k, v) in row.items()])
+            clean_row = {k: preProcess(v) for (k, v) in row.items()}
             clean_row['name'] = ' '.join([clean_row['FirstName'], 
                                           clean_row['LastName']])
             if not clean_row['name'] :
@@ -76,6 +74,11 @@ def readData(filename):
             if not clean_row['address'] :
                 clean_row['address'] = None
             row_id = int(row['ID'])
+
+            for k, v in clean_row.items():
+                if not v:
+                    clean_row[k] = None
+
             data_d[row_id] = clean_row
 
     return data_d
@@ -104,7 +107,6 @@ else:
         {'field' : 'City', 'type' : 'ShortString'},
         {'field' : 'State', 'type' : 'ShortString'},
         {'field' : 'Zip', 'type' : 'ShortString'},
-        {'field' : 'Title', 'type' : 'ShortString'},
         {'field' : 'Phone', 'type' : 'ShortString'},
         {'field' : 'RedactionRequested', 'type' : 'Categorical',
          'categories' : ['true', 'false']}]
@@ -113,7 +115,7 @@ else:
     deduper = dedupe.Dedupe(fields)
 
     # To train dedupe, we feed it a sample of records.
-    deduper.sample(data_d, 1500)
+    deduper.sample(data_d)
 
 
     # If we have training data saved from a previous run of dedupe,
@@ -153,20 +155,11 @@ print('blocking...')
 
 # ## Clustering
 
-# Find the threshold that will maximize a weighted average of our precision and recall. 
-# When we set the recall weight to 2, we are saying we care twice as much
-# about recall as we do precision.
-#
-# If we had more data, we would not pass in all the blocked data into
-# this function but a representative sample.
-
-threshold = deduper.threshold(data_d, recall_weight=2)
-
 # `match` will return sets of record IDs that dedupe
 # believes are all referring to the same entity.
 
 print('clustering...')
-clustered_dupes = deduper.match(data_d, threshold)
+clustered_dupes = deduper.match(data_d, threshold=0.5)
 
 print('# duplicate sets', len(clustered_dupes))
 
@@ -180,11 +173,9 @@ cluster_id = 0
 for (cluster_id, cluster) in enumerate(clustered_dupes):
     id_set, scores = cluster
     cluster_d = [data_d[c] for c in id_set]
-    canonical_rep = dedupe.canonicalize(cluster_d)
     for record_id, score in zip(id_set, scores) :
         cluster_membership[record_id] = {
             "cluster id" : cluster_id,
-            "canonical representation" : canonical_rep,
             "confidence": score
         }
 
@@ -199,9 +190,6 @@ with open(output_file, 'w') as f_output:
         heading_row = next(reader)
         heading_row.insert(0, 'confidence_score')
         heading_row.insert(0, 'Cluster ID')
-        canonical_keys = canonical_rep.keys()
-        for key in canonical_keys:
-            heading_row.append('canonical_' + key)
         
         writer.writerow(heading_row)
 
@@ -209,15 +197,10 @@ with open(output_file, 'w') as f_output:
             row_id = int(row[0])
             if row_id in cluster_membership :
                 cluster_id = cluster_membership[row_id]["cluster id"]
-                canonical_rep = cluster_membership[row_id]["canonical representation"]
                 row.insert(0, cluster_membership[row_id]['confidence'])
                 row.insert(0, cluster_id)
-                for key in canonical_keys:
-                    row.append(canonical_rep[key].encode('utf8'))
             else:
                 row.insert(0, None)
                 row.insert(0, singleton_id)
                 singleton_id += 1
-                for key in canonical_keys:
-                    row.append(None)
             writer.writerow(row)
