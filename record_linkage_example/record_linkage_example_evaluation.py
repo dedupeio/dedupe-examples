b88d222dc95e3e8af4c23025f1d3268cb771e9fa
Forest Gregg <fgregg@uchicago.edu>
2018-10-15 11:07:29 -0500

    fix evaluation

--- record_linkage_example/record_linkage_example_evaluation.py
+++ record_linkage_example/record_linkage_example_evaluation.py
@@ -18,29 +18,31 @@ def evaluateDuplicates(found_dupes, true_dupes):
     print(len(true_positives) / float(len(true_dupes)))
 
 
-def dupePairs(filename, rowname) :
-    dupe_d = collections.defaultdict(list)
+def linkPairs(filename, rowname) :
+    link_d = {}
 
     with open(filename) as f:
         reader = csv.DictReader(f, delimiter=',', quotechar='"')
         for i, row in enumerate(reader):
-            dupe_d[row[rowname]].append(i)
+            source_file, link_id = row['source file'], row[rowname]
+            if link_id:
+                if link_id not in link_d:
+                    link_d[link_id] = collections.defaultdict(list)
 
-    if 'x' in dupe_d :
-        del dupe_d['x']
+                link_d[link_id][source_file].append(i)
 
-    dupe_s = set([])
-    for (unique_id, cluster) in dupe_d.items():
-        if len(cluster) > 1:
-            for pair in itertools.combinations(cluster, 2):
-                dupe_s.add(frozenset(pair))
+    link_s = set()
 
-    return dupe_s
+    for members in link_d.values():
+        for pair in itertools.product(*members.values()):
+            link_s.add(frozenset(pair))
+
+    return link_s
 
 clusters = 'data_matching_output.csv'
 
-true_dupes = dupePairs(clusters, 'unique_id')
-test_dupes = dupePairs(clusters, 'Cluster ID')
+true_dupes = linkPairs(clusters, 'unique_id')
+test_dupes = linkPairs(clusters, 'Cluster ID')
 
 evaluateDuplicates(test_dupes, true_dupes)
 
