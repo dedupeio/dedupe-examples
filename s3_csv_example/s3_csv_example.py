0775f39fdfb45e2794b11265c0d8d11115449534
Rob Kraft <rob@robkraft.org>
2021-04-08 19:10:11 -0500

    Making things more dynamic - Response Ref ID

--- s3_csv_example/s3_csv_example.py
+++ s3_csv_example/s3_csv_example.py
@@ -29,7 +29,19 @@ def preProcess(column):
     if not column:
         column = None
     return column
-
+def preProcessCase(column):
+    """
+    Do a little bit of data cleaning with the help of Unidecode and Regex.
+    Things like casing, extra spaces, quotes and new lines can be ignored.
+    """
+    column = unidecode(column)
+    column = re.sub('  +', ' ', column)
+    column = re.sub('\n', ' ', column)
+    column = column.strip().strip('"').strip("'").strip()
+    # If data is missing, indicate that by setting the value to `None`
+    if not column:
+        column = None
+    return column
 def readMappings(filename):
 
 
@@ -38,7 +50,7 @@ def readMappings(filename):
         reader = csv.DictReader(f)
         for row in reader:
             fldSource = str(row['Source'])
-            clean_row = [(k, preProcess(v)) for (k, v) in row.items()]
+            clean_row = [(k, preProcessCase(v)) for (k, v) in row.items()]
             data_d[fldSource] = dict(clean_row)
 
     return data_d
@@ -93,7 +105,7 @@ if __name__ == '__main__':
     
     fieldNameFileNo = 'FileNo' #not dynamic - we totally control this
     fieldNameFileName = 'FileName' #not dynamic - we totally control this
-    fieldNameIdInSource = 'IdInSource' #not dynamic - we totally control this
+    fieldNameIdInSource = 'Key' #not dynamic - we totally control this
     fieldNameClusterId = 'ClusterId' #not dynamic - we totally control this 
     fieldNameConfidence = 'ConfidenceScore' #not dynamic - we totally control this 
     FileSource = "comebackkc1"
@@ -148,39 +160,86 @@ if __name__ == '__main__':
     else: #FileSource = "comebackkc1"
         s3files.append('C:/Users/robkr/Downloads/ResponseExport-KCRegionalCOVID19VaccinationSurvey-20210323 (1)/ResponseExport-KCRegionalCOVID19VaccinationSurvey-20210323.csv')
         #csv_header = 'Receipt Number,Response Reference ID,Respondent Email,URL submitted from,Form version submitted in,Response Submission DateTime,Time Taken To Complete (seconds),External ID,External Status,Are you planning to receive the COVID-19 vaccine?,What makes you uncertain about receiving the COVID-19 vaccine?,When would you like to get your first dose of the COVID-19 vaccine?,What state do you live in?,Which Kansas county do you live in?,"Do you live within the city limits of Kansas City, MO?",Which Missouri county do you live in?,What is your zip code?,First Name,Last Name,Street Address,City,"Phone number (please enter numbers only, no dashes, spaces, or parentheses)",Email address,What is your preferred method of contact?,We need your permission to contact you about COVID-19 testing and vaccination.,Is it okay for us to leave you a voicemail?,Sex,Age,Race/ethnicity (check as many as apply),"Do you have any pre-existing medical conditions or do you have any conditions that put you at increased risk of severe illness? (i.e. immunocompromised, diabetes, chronic lung conditions, cardiovascular disease, morbid obesity, etc.)","Do you live in or visit often crowded living settings? (For example, a supportive care facility, assisted living facility, group home, homeless shelter, or correctional setting)","How many members, including yourself, live in your household?",Do you have a history of any of the following pre-existing medical conditions?,Are you immuno-compromised?,What is your work status?,"What is your work zip code, either where you normally work in the office or on location or the zip code of your employers primary office?",Are you employed by any of the following types of patient-facing organizations?,What is the name of the health care provider for which you work?,Are you responsible for or in a position to influence vaccination planning for your employer or another organization?,What is the name of your employer or other organization?,What is your job title or role at your organization?'
-#new stuff here
+
     csv_out = 'C:/Users/robkr/Source/Repos/dedupe-examples/s3_csv_example/combinedfile.csv'
 
+
+    fileInfos = { "f":[]}
+    fileInfo = {0: []}
+    fileInfo["source"]=""
+    fileInfo["num"]=0
+    fileInfos["f"].append(fileInfo)
+
+    fileprefix = 'a_'
+#4Start############# This ugly section exists to remove the extra header that is in some csv files #################
     fileno = 1 #Used to make the key independent per record when we combine multiple input files
     count = -1
-    csv_merge = open(csv_out, 'w')
     #replacing Receipt Number with IDinSource in this file!!!
     #TODO - Read in Headers and do a replace of header names based on the mappings.
-    csv_headerNew = fieldNameIdInSource + ',Response Reference ID,Respondent Email,URL submitted from,Form version submitted in,Response Submission DateTime,Time Taken To Complete (seconds),External ID,External Status,Are you planning to receive the COVID-19 vaccine?,What makes you uncertain about receiving the COVID-19 vaccine?,When would you like to get your first dose of the COVID-19 vaccine?,What state do you live in?,Which Kansas county do you live in?,"Do you live within the city limits of Kansas City, MO?",Which Missouri county do you live in?,Zip,FirstName,LastName,Street Address,City,"Phone",Email,What is your preferred method of contact?,We need your permission to contact you about COVID-19 testing and vaccination.,Is it okay for us to leave you a voicemail?,Sex,Age,Race/ethnicity (check as many as apply),"Do you have any pre-existing medical conditions or do you have any conditions that put you at increased risk of severe illness? (i.e. immunocompromised, diabetes, chronic lung conditions, cardiovascular disease, morbid obesity, etc.)","Do you live in or visit often crowded living settings? (For example, a supportive care facility, assisted living facility, group home, homeless shelter, or correctional setting)","How many members, including yourself, live in your household?",Do you have a history of any of the following pre-existing medical conditions?,Are you immuno-compromised?,What is your work status?,"What is your work zip code, either where you normally work in the office or on location or the zip code of your employers primary office?",Are you employed by any of the following types of patient-facing organizations?,What is the name of the health care provider for which you work?,Are you responsible for or in a position to influence vaccination planning for your employer or another organization?,What is the name of your employer or other organization?,What is your job title or role at your organization?'
+    for file in s3files:
+        firstpos=file.rfind("/")
+        lastpos=len(file)
+        filenameonly = file[firstpos+1:lastpos]
+        if filenameonly.startswith("Response"):
+            FileSource="comebackkc1"
+            fileInfo = {fileno: []}
+            fileInfo["source"]=FileSource
+            fileInfo["num"]=fileno
+            fileInfo["unq"]=header_map[FileSource]["Unique ID"]
+            fileInfos["f"].append(fileInfo)
+        csv_stripextraheader = open(fileprefix + filenameonly, 'w')
+        csv_in = open(file,"r",encoding='utf-8') #https://stackoverflow.com/questions/49562499/how-to-fix-unicodedecodeerror-charmap-codec-cant-decode-byte-0x9d-in-posit
+        for line in csv_in:
+            if (FileSource=="comebackkc1") and (count < 0): #line.startswith(csv_header):
+                count = count + 1 #skip this line - remove it from the rewritten file
+                continue
+            csv_stripextraheader.write(line)
+            print(count)
+            count = count + 1
+        csv_in.close()
+        fileno = fileno + 1
+    csv_stripextraheader.close()
+#4End############# This ugly section exists to remove the extra header that is in some csv files #################
+
+#5Start############# Combine multiple input files into one single file with consistent column headers #################
+    fileno = 1 #Used to make the key independent per record when we combine multiple input files
+    count = 0
+    csv_merge = open(csv_out, 'w', newline='')
+    #replacing Receipt Number with IDinSource in this file!!!
+    #TODO - Read in Headers and do a replace of header names based on the mappings.
+    #csv_headerNew = fieldNameIdInSource + ',Response Reference ID,Respondent Email,URL submitted from,Form version submitted in,Response Submission DateTime,Time Taken To Complete (seconds),External ID,External Status,Are you planning to receive the COVID-19 vaccine?,What makes you uncertain about receiving the COVID-19 vaccine?,When would you like to get your first dose of the COVID-19 vaccine?,What state do you live in?,Which Kansas county do you live in?,"Do you live within the city limits of Kansas City, MO?",Which Missouri county do you live in?,Zip,FirstName,LastName,Street Address,City,"Phone",Email,What is your preferred method of contact?,We need your permission to contact you about COVID-19 testing and vaccination.,Is it okay for us to leave you a voicemail?,Sex,Age,Race/ethnicity (check as many as apply),"Do you have any pre-existing medical conditions or do you have any conditions that put you at increased risk of severe illness? (i.e. immunocompromised, diabetes, chronic lung conditions, cardiovascular disease, morbid obesity, etc.)","Do you live in or visit often crowded living settings? (For example, a supportive care facility, assisted living facility, group home, homeless shelter, or correctional setting)","How many members, including yourself, live in your household?",Do you have a history of any of the following pre-existing medical conditions?,Are you immuno-compromised?,What is your work status?,"What is your work zip code, either where you normally work in the office or on location or the zip code of your employers primary office?",Are you employed by any of the following types of patient-facing organizations?,What is the name of the health care provider for which you work?,Are you responsible for or in a position to influence vaccination planning for your employer or another organization?,What is the name of your employer or other organization?,What is your job title or role at your organization?'
+    csv_headerNew = 'FirstName,LastName,Email,City,Phone,Zip,Unique ID,Key'
     csv_merge.write(fieldNameFileName + ',' + fieldNameFileNo + ',' + csv_headerNew)
     csv_merge.write('\n')
     for file in s3files:
         firstpos=file.rfind("/")
         lastpos=len(file)
         filenameonly = file[firstpos+1:lastpos]
-        csv_in = open(file,"r",encoding='utf-8') #https://stackoverflow.com/questions/49562499/how-to-fix-unicodedecodeerror-charmap-codec-cant-decode-byte-0x9d-in-posit
-        for line in csv_in:
-            if count < 1: #line.startswith(csv_header):
+        if filenameonly.startswith("Response"):
+            FileSource="comebackkc1"
+        #https://stackoverflow.com/questions/49562499/how-to-fix-unicodedecodeerror-charmap-codec-cant-decode-byte-0x9d-in-posit
+        #I had sever 0x92 - https://stackoverflow.com/questions/37083687/unicodedecodeerror-ascii-codec-cant-decode-byte-0x92
+        with open(fileprefix + filenameonly,"r",encoding='windows-1252') as f_input:
+            reader = csv.DictReader(f_input, dialect='excel')
+            for row in reader:
+                print(count)
                 count = count + 1
-                continue
-            csv_merge.write(filenameonly + ',' + str(fileno) + ',' + line)
-            print(count)
-            count = count + 1
-        csv_in.close()
+                outrow = (filenameonly + ',' + str(fileno) + ',"' + row[header_map[FileSource]["FirstName"]]
+                + '","' + row[header_map[FileSource]["LastName"]] + '",' + row[header_map[FileSource]["Email"]]
+                + ',"' + row[header_map[FileSource]["City"]] + '",' + row[header_map[FileSource]["Phone"]]
+                + ',' + row[header_map[FileSource]["Zip"]] + ',' + row[header_map[FileSource]["Unique ID"]]
+                + ',' + row[header_map[FileSource]["Key"]] + '\n')
+                print(outrow)
+                csv_merge.write(outrow)
         fileno = fileno + 1
     csv_merge.close()
+#5End############# Combine multiple input files into one single file with consistent column headers #################
 
 
     #results = pd.DataFrame([])
 #https://www.techbeamers.com/merge-multiple-csv-files/
     #csv_header = 'Receipt Number,Response Reference ID,Respondent Email,URL submitted from,Form version submitted in,Response Submission DateTime,Time Taken To Complete (seconds),External ID,External Status,Are you planning to receive the COVID-19 vaccine?,What makes you uncertain about receiving the COVID-19 vaccine?,When would you like to get your first dose of the COVID-19 vaccine?,What state do you live in?,Which Kansas county do you live in?,"Do you live within the city limits of Kansas City, MO?",Which Missouri county do you live in?,What is your zip code?,First Name,Last Name,Street Address,City,"Phone number (please enter numbers only, no dashes, spaces, or parentheses)",Email address,What is your preferred method of contact?,We need your permission to contact you about COVID-19 testing and vaccination.,Is it okay for us to leave you a voicemail?,Sex,Age,Race/ethnicity (check as many as apply),"Do you have any pre-existing medical conditions or do you have any conditions that put you at increased risk of severe illness? (i.e. immunocompromised, diabetes, chronic lung conditions, cardiovascular disease, morbid obesity, etc.)","Do you live in or visit often crowded living settings? (For example, a supportive care facility, assisted living facility, group home, homeless shelter, or correctional setting)","How many members, including yourself, live in your household?",Do you have a history of any of the following pre-existing medical conditions?,Are you immuno-compromised?,What is your work status?,"What is your work zip code, either where you normally work in the office or on location or the zip code of your employers primary office?",Are you employed by any of the following types of patient-facing organizations?,What is the name of the health care provider for which you work?,Are you responsible for or in a position to influence vaccination planning for your employer or another organization?,What is the name of your employer or other organization?,What is your job title or role at your organization?'
 
-#end new
 
     print('importing data ...')
     data_d = {}
@@ -196,7 +255,7 @@ if __name__ == '__main__':
     if FileSource == "dedupesample":
         outputfieldnames = reader.fieldnames
     else: #FileSource = "comebackkc1"
-        fieldNameSourceFileUniqueId = 'Response Reference ID' #this is probably different for each file - a GUID in some cases
+        fieldNameSourceFileUniqueId = header_map[FileSource]["Unique ID"] #this is probably different for each file - a GUID in some cases
         outputfieldnames = [fieldNameFileName,fieldNameFileNo,fieldNameIdInSource,fieldNameSourceFileUniqueId,'FirstName', 'LastName','Email','City','Phone','Zip']
         # If a settings file already exists, we'll just load that and skip training
     if os.path.exists(settings_file):
@@ -296,8 +355,19 @@ if __name__ == '__main__':
             row_id = str(int(row[fieldNameFileNo])) + '.' + str(int(row[fieldNameIdCol]))
             row.update(cluster_membership[row_id])
             
+                        #   newrow = {fieldNameFileName: filenameonly, fieldNameFileNo: str(fileno),
+                        # 'Unique ID': row[header_map[agency]["Unique ID"]],
+                        #'Key':row[header_map[agency]["Key"]],
+                        #'FirstName': row[header_map[agency]["FirstName"]],
+                        #'LastName': row[header_map[agency]["LastName"]],
+                        # 'City': row[header_map[agency]["City"]],
+                        #'Zip': row[header_map[agency]["Zip"]],
+                        # 'Phone': row[header_map[agency]["Phone"]],
+                        #'Email': row[header_map[agency]["Email"]]}
+
+
             newrow = {fieldNameFileName: row[fieldNameFileName], fieldNameFileNo: row[fieldNameFileNo],
-                     fieldNameIdInSource: row[fieldNameIdInSource], fieldNameSourceFileUniqueId:row[fieldNameSourceFileUniqueId],
+                     fieldNameIdInSource: row[fieldNameIdInSource], fieldNameSourceFileUniqueId:row["Unique ID"],
                     'FirstName': row['FirstName'], 'LastName': row['LastName']
                      , 'City': row['City'], 'Zip': row['Zip']
                      , 'Phone': row['Phone'], 'Email': row['Email']
